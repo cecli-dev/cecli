164ffc49352556cd1f953abaf8275093166769f8
Your Name <you@example.com>
2026-04-05 07:43:27 -0400

    Update model_request_parser for simple send to include tools argument

--- cecli/models.py
+++ cecli/models.py
@@ -1120,7 +1120,7 @@ class Model(ModelSettings):
         from cecli.exceptions import LiteLLMExceptions
 
         litellm_ex = LiteLLMExceptions()
-        messages = model_request_parser(self, messages)
+        messages = model_request_parser(self, messages, None)
         retry_delay = 0.125
         if self.verbose:
             dump(messages)
